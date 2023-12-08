eb3b18348b87627e41949e84bfe5a5853793c3da
William Word <weword@gmail.com>
2023-12-08 14:35:17 -0800

    add crispy forms and bootstrap5

--- dj4e/settings.py
+++ dj4e/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'ads',
     "social_django",
     'crispy_forms',
+    'crispy_bootstrap5',  # Forgetting this was probably your error
 
     'django_extensions',
 ]
@@ -126,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
